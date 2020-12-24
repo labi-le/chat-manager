0997b9e421e0ea9de15afa81002b811a7f6527c6
labile <1a6i1e@gmail.com>
2020-12-24 06:00:13 +0400

    update

--- bot.php
+++ bot.php
@@ -3,37 +3,123 @@
 // namespace DigitalStars\SimpleVK;
 
 use DigitalStars\SimpleVK\LongPoll as longpool;
+use DigitalStars\SimpleVK\Message;
 use DigitalStars\SimpleVK\SimpleVK as vk;
 
 require_once('./vendor/digitalstars/simplevk/autoload.php');
 
-class ChatManager extends vk
+class ChatManager extends longpool
 {
-    public function say($text)
-    {
-        // parent::msg('heyooo')->send();
-        return $text;
-    }
+    public $data;
+    public $initData;
 
+    private function say($text)
+    {
+        $this->msg($text)->send();
+    }
 
     public static function create($token, $version, $also_version = null)
     {
         return new self($token, $version, $also_version);
     }
 
-    public function run($token, $version, $also_version = null)
+    public function run()
     {
-        $lp = longpool::create($token, $version, $also_version);
-        $lp->listen(function ($data) use ($lp) {                       // Получение событий из LongPool
-            $lp->initVars($id, $user_id, $type, $message, $payload, $msg_id, $attachments);   // Парсинг полученных событий
-            if ($type == 'message_new') {                                     // Если событие - новое сообщение
-                $lp->msg("Тестовое сообщение")->send();                       // Отправка ответного сообщения
-            }
+        $this->isMultiThread(true);  // ну потоки же круто почему нет как бы
+
+        $lexicalThis = $this; // this is lexical зато можно rename
+        $this->listen(function () use ($lexicalThis) {                       // Получение событий из LongPool
+            // $data = $this->initVars($id, $user_id, $type, $message, $payload, $msg_id, $attachments);   // Парсинг полученных событий
+
+            $data = $this->data;
+            $this->initVars = call_user_func(function () use ($data) {
+                $_obj = $data['object'];
+                $_msg = $_obj['message'] ?? null;
+
+                $new_data = null;
+                isset($_msg) ? $new_data = $_msg : $new_data = $_obj;
+                $new_data['text_lower'] = mb_strtolower($new_data['text']);
+
+                return $new_data;
+            });
+
+            print_r($this->initVars);
+            $lexicalThis->handleType($type);
         });
     }
 
+    private function handleType(&$type): void
+    {
+        if (method_exists($this, $type)) $this->$type($this->initVars);
+    }
+
+    /**
+     * Event message_new --> action 
+     */
+    protected function handleAction(): void
+    {
+        $action = $this->data['action'];
+        $type = $action['type'];
+
+        if (method_exists($this, $type)) $this->$type($action['member_id']);
+    }
+
+    // Действие при приглашении пользователя
+    private function chat_invite_user($member_id)
+    {
+    }
+
+    // Действие при приглашении пользователя по ссылке
+    private function chat_invite_user_by_link($member_id)
+    {
+    }
+
+    // Действие при выходе юзера из беседы\кик
+    private function chat_kick_user($member_id)
+    {
+    }
+
+    // Действие при обновлении фотки беседы
+    private function chat_photo_update($member_id)
+    {
+    }
+
+    // Действие при удалении фото беседы
+    private function chat_photo_remove($member_id)
+    {
+    }
+
+    // Действие при закреплении сообщения
+    private function chat_pin_message($member_id)
+    {
+    }
+
+    // Действие при откреплении
+    private function chat_unpin_message()
+    {
+    }
+
+    /**
+     * Event message_new
+     */
+    public function message_new($data): void
+    {
+        print_r($this->initVars);
+        if ($data['text'] == 'hi') {
+            $this->say('Hello nigger');
+        }
+
+        if (isset($data['action'])) $this->handleAction();
+    }
+
+    /**
+     * Event message_event
+     */
+    public function message_event($data): void
+    {
+    }
 }
 
-$vk = ChatManager::run('access_token', 5.126);
+$vk = ChatManager::create('access_token', 5.126)->run();
 
 var_dump($vk);
